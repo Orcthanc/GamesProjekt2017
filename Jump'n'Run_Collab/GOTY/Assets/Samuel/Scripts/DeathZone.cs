47831bdf8739a301cd61c681835ce483b712c30e
Orcthanc <orcthanclp@gmail.com>
2018-01-15 16:23:43 +0100

    Added the current Version of the game to the github

---
+++ Jump'n'Run_Collab/GOTY/Assets/Samuel/Scripts/DeathZone.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathZone : MonoBehaviour {
+
+	public void OnDrawGizmos()
+    {
+        Gizmos.DrawIcon(transform.position, "deathzone", true);
+    }
+    public void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawIcon(transform.position, "deathzone", true);
+    }
+}
---
+++ Jump'n'Run_Collab/GOTY/Assets/Samuel/Scripts/SpawnPoint.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPoint : MonoBehaviour {
+
+	public void OnDrawGizmos()
+    {
+        Gizmos.DrawIcon(transform.position, "spawnpoint");
+    }
+}
