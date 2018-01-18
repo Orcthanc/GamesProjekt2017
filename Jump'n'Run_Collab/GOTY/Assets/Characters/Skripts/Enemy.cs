fbe4f92eac75e29a3f1a545a1fee6a97e4fc5a79
Orcthanc <orcthanclp@gmail.com>
2018-01-18 15:24:37 +0100

    Updated the abstract Enemy

--- Jump'n'Run_Collab/GOTY/Assets/Characters/Skripts/Enemy.cs
+++ Jump'n'Run_Collab/GOTY/Assets/Characters/Skripts/Enemy.cs
@@ -4,14 +4,46 @@ using UnityEngine;
 
 public abstract class Enemy : MonoBehaviour {
 
+    public float shotCooldown = 1f;
+    public readonly float standardShotCooldown = 1f;
+
+    public bool agro = false;
+
+    private int hp = 100;
+
+    public int Damage
+    {
+        get{
+            return hp;
+        }
+
+        set{
+            hp -= value;
+        }
+    }
+
     public void Update()
     {
-        Move();
+        if (agro)
+        {
+            Move();
+            shotCooldown -= Time.deltaTime;
+            if(shotCooldown < 0)
+            {
+                shotCooldown += standardShotCooldown;
+                Shoot();
+            }
+        }
+        else
+        {
+            agro = CheckAgro();
+        }
     }
 
-    public void Move()
-    {
+    public abstract bool CheckAgro();
 
-    }
+    public abstract void Move();
+
+    public abstract void Shoot();
 
 }
