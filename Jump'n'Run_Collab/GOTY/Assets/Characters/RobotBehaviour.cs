4aa871366a29e6914787597df6c4fcc4d9c587b9
LucaTuerk <lucatuerk@googlemail.com>
2018-01-18 12:36:54 +0100

    Added Animations

---
+++ Jump'n'Run_Collab/GOTY/Assets/Characters/RobotBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Animations;
+
+public class RobotBehaviour : MonoBehaviour {
+
+    private Animator anim;
+    private string[] names;
+    int i;
+
+	void Start () {
+        anim = gameObject.GetComponent<Animator>();
+
+        int i = 0;
+        AnimationClip[] clips = anim.runtimeAnimatorController.animationClips;
+        names = new string[clips.Length];
+        foreach(AnimationClip clip in clips){
+            names[i++] = clip.name;
+           // Debug.Log(names[i - 1]);
+        }
+        anim.Play("Walk");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        anim.Update(Time.deltaTime);
+
+        ++i;
+        if((i %= 500) == 0){
+            Debug.Log("-->Shoot");
+            anim.Play("Shoot");
+        }
+
+   	}
+}
---
+++ Jump'n'Run_Collab/GOTY/Assets/Guns/RifleBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RifleBehaviour : MonoBehaviour {
+
+    private Animator anim;
+    private string[] names;
+	// Use this for initialization
+    void Start()
+    {
+        anim = gameObject.GetComponentInChildren<Animator>();
+        Debug.Log(anim.ToString());
+
+        int i = 0;
+        AnimationClip[] clips = anim.runtimeAnimatorController.animationClips;
+        names = new string[clips.Length];
+        foreach (AnimationClip clip in clips)
+         {
+             names[i++] = clip.name;
+             Debug.Log(names[i - 1]);
+         }
+        anim.Play("Shoot");
+
+        Debug.Log(AnimatorIsPlaying("Empty"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.Update(Time.deltaTime);
+    }
+
+    bool AnimatorIsPlaying()
+    {
+        return anim.GetCurrentAnimatorStateInfo(0).length >
+               anim.GetCurrentAnimatorStateInfo(0).normalizedTime;
+    }
+
+    bool AnimatorIsPlaying(string stateName)
+    {
+        return AnimatorIsPlaying() && anim.GetCurrentAnimatorStateInfo(0).IsName(stateName);
+    }
+}
