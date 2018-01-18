75bf65e40a9796631d79bcf46be72a2054aeb9d9
Orcthanc <orcthanclp@gmail.com>
2018-01-18 18:07:35 +0100

    + FlyingRobot is now turning towards the players position
    - Hopefully removed the folder .vs/

--- Jump'n'Run_Collab/GOTY/Assets/Characters/Skripts/FlyingRobotBehaviour.cs
+++ Jump'n'Run_Collab/GOTY/Assets/Characters/Skripts/FlyingRobotBehaviour.cs
@@ -5,11 +5,23 @@ using UnityEngine;
 public class FlyingRobotBehaviour : Enemy {
 
     public GameObject player;
-	
+    public float rotationSpeed = 100;
+    private Transform target;
+
+    public new void Start()
+    {
+        target = new GameObject().transform;
+    }
+
     public override void Move()
     {
-        transform.LookAt(player.transform);
-        transform.Rotate(new Vector3(0, 180, 0), Space.Self);
+        target.position = transform.position;
+        target.LookAt(player.transform);
+        target.Rotate(new Vector3(0, 180, 0), Space.Self);
+
+        float step = rotationSpeed * Time.deltaTime;
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, target.rotation, step);
+
     }
 
     public override bool CheckAgro()
@@ -19,6 +31,5 @@ public class FlyingRobotBehaviour : Enemy {
 
     public override void Shoot()
     {
-        Debug.Log(Random.value);
     }
 }
