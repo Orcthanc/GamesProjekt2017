2ed40d38b309c7bcea6f9cb064560779a22ed2c0
Orcthanc <orcthanclp@gmail.com>
2018-01-18 15:36:59 +0100

    Started work on the flying enemy skript

---
+++ Jump'n'Run_Collab/GOTY/Assets/Characters/Skripts/FlyingRobot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingRobot : Enemy {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    public override void Move()
+    {
+
+    }
+
+    public override bool CheckAgro()
+    {
+        return true;
+    }
+
+    public override void Shoot()
+    {
+        throw new System.NotImplementedException();
+    }
+}
