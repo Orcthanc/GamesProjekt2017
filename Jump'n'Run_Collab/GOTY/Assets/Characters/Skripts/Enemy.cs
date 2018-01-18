5e844c21119e4c17110db668d965cf5aba35b0cc
Orcthanc <orcthanclp@gmail.com>
2018-01-18 14:04:01 +0100

    Added a abstract Enemy class that all enemy-scripts should inherit

---
+++ Jump'n'Run_Collab/GOTY/Assets/Characters/Skripts/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour {
+
+    public void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+
+    }
+
+}
