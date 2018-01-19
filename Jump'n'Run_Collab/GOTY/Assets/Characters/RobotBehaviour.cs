16c0bdaac325c1811f3f892c8667525e19302ad4
LucaTuerk <lucatuerk@googlemail.com>
2018-01-19 15:51:02 +0100

    - Added NavMesh Components - Added Prefabs - Added Projectile Model - Started Robot Behaviour

--- Jump'n'Run_Collab/GOTY/Assets/Characters/RobotBehaviour.cs
+++ Jump'n'Run_Collab/GOTY/Assets/Characters/RobotBehaviour.cs
@@ -1,36 +1,88 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEditor.Animations;
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Security;
 
-public class RobotBehaviour : MonoBehaviour {
+public class RobotBehaviour : MonoBehaviour{
 
     private Animator anim;
+    private NavMeshAgent agent;
+    private Transform player;
+
     private string[] names;
     int i;
 
-	void Start () {
-        anim = gameObject.GetComponent<Animator>();
+    public void Start()
+    {
+        anim = gameObject.GetComponentInChildren<Animator>();
+        agent = gameObject.GetComponent<NavMeshAgent>();
+        player = GameObject.FindWithTag("Player").transform;
+        agent.autoTraverseOffMeshLink = true;
+    }
 
-        int i = 0;
-        AnimationClip[] clips = anim.runtimeAnimatorController.animationClips;
-        names = new string[clips.Length];
-        foreach(AnimationClip clip in clips){
-            names[i++] = clip.name;
-           // Debug.Log(names[i - 1]);
-        }
-        anim.Play("Walk");
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    public void Update()
+    {
+        i++;
         anim.Update(Time.deltaTime);
 
-        ++i;
-        if((i %= 500) == 0){
-            Debug.Log("-->Shoot");
-            anim.Play("Shoot");
+        Destinate(player);
+        LookAt(agent.nextPosition);
+
+        if (agent.speed > 0.0f){
+            setWalk();
+        }
+    
+
+        if(Distance(player) < 5.0f){
+
+            Destinate(gameObject.transform);
+            setAim();
+            LookAt(player);
+
+            if ((i %= 200) == 0)
+            {
+                anim.SetTrigger("Shoot");
+            }
         }
 
    	}
+
+    public void setWalk(){
+        anim.SetBool("Walk", true);
+        anim.SetBool("Aim", false);
+    }
+
+    public void setAim(){
+        anim.SetBool("Walk", false);
+        anim.SetBool("Aim", true);
+    }
+
+  //  override
+    public bool CheckAgro(){
+        return false;
+    }
+
+   // override
+    public void Move() {
+        
+    }
+
+    //override
+    public void Shoot() {
+        
+    }
+
+    bool SeesPlayer(){
+        return false;
+    }
+
+    void LookAt(Transform target) { LookAt(target.position); }
+    void LookAt(Vector3 target) { 
+        gameObject.transform.LookAt(new Vector3 (target.x, gameObject.transform.position.y,target.z)); 
+    }
+    void Destinate(Vector3 target) { agent.destination = target; }
+    void Destinate(Transform target) { agent.destination = target.position; }
+    float Distance(Transform target) { return Distance(target.position);}
+    float Distance (Vector3 target) { return Vector3.Distance(gameObject.transform.position, target);}
+
 }
