2ff03714fed913ed6072ca32e285bb2e5c9b8edb
Orcthanc <samuel@knoethig.net>
2018-01-19 02:12:53 +0100

    + Added a script for the FlyingRobotProjectile

---
+++ Jump'n'Run_Collab/GOTY/Assets/Characters/Prefabs/Enemies/FlyingBot/FlyingRobotProjectile.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingRobotProjectile : Projectile {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+}
