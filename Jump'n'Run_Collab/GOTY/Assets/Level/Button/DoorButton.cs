c30cfe5edc43ae2efa2b274bd0528cd4a8d0ab8b
LucaTuerk <lucatuerk@googlemail.com>
2018-01-24 22:09:34 +0100

    Added Buttons

---
+++ Jump'n'Run_Collab/GOTY/Assets/Level/Button/DoorButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+public class DoorButton : ButtonScript
+{
+    public GameObject door;
+
+    public override void Action(){
+        Debug.Log("DESTROYING DOOR");
+        Destroy(door);
+    }
+
+    void actionTrigger()
+    {
+        if (pushTrigger)
+        {
+            Debug.Log("Trigger");
+            pushTrigger = false;
+            Action();
+        }
+    }
+}
+
---
+++ Jump'n'Run_Collab/GOTY/Assets/Level/Button/SpawnButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+public class SpawnButton : ButtonScript{
+
+    public GameObject prefab;
+    public GameObject posEmpty;
+
+    public override void Action(){
+        Instantiate(prefab,posEmpty.transform);
+    }
+}
