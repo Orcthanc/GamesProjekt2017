d212aa1f5cf0e492d227cef42524b0c7a6f4ff32
LucaTuerk <lucatuerk@googlemail.com>
2018-01-24 19:53:42 +0100

    - Worked on Level 	* Added Soyouz and Sputnik

---
+++ Jump'n'Run_Collab/GOTY/Assets/scr.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr : MonoBehaviour {
+
+	void Start () {
+        removeColliders(gameObject.transform);
+	}
+
+    void removeColliders(Transform go){
+        int count = go.GetChildCount();
+        Collider[] colliders = go.GetComponents<Collider>();
+
+        foreach(Collider c in colliders){
+            c.enabled = false;
+        }
+
+        for (int i = 0; i < count; i++){
+            removeColliders(go.transform.GetChild(i));
+        }
+    }
+}
