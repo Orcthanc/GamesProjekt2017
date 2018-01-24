dc113621be15184a34889dd5ab17b05f1ca40289
Orcthanc <orcthanclp@gmail.com>
2018-01-24 18:55:55 +0100

    + Added a MovingPlatform based on a sin function for smoother movements
    + Added an option to delete the platform after it reaches it's goal

---
+++ Jump'n'Run_Collab/GOTY/Assets/Level/MovingPlatform/MovingPlattform.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MovingPlattform : MonoBehaviour {
+
+    public Transform start, goal;
+    public float speed;
+    public bool destroyOnTarget;
+
+    private Vector3 m_start;
+    private Vector3 m_toGoal;
+
+    private int startFrame;
+    private bool dontMove;
+
+	// Use this for initialization
+	void Start () {
+        m_start = start.position;
+        m_toGoal = goal.position - start.position;
+
+        startFrame = Time.frameCount;
+
+        Destroy(start.gameObject);
+        Destroy(goal.gameObject);
+	}
+
+    int framesSinceStart()
+    {
+        return Time.frameCount - startFrame;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!dontMove)
+        {
+            transform.position = m_start + m_toGoal * (-Mathf.Cos(framesSinceStart() / (1000 / speed)) / 2 + 0.5f);
+            if (destroyOnTarget && (framesSinceStart() / (1000 / speed)) > Mathf.PI)
+            {
+                dontMove = true;
+                StartCoroutine(destroy());
+            }
+        }
+	}
+
+    IEnumerator destroy()
+    {
+        yield return new WaitForSeconds(3);
+        Destroy(gameObject);
+    }
+}
