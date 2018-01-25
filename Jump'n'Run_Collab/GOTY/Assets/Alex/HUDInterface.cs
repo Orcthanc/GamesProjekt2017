57de8b101cce2054b3873b5a020742a2bd2885f5
Goodvinn <35471848+Goodvinn@users.noreply.github.com>
2018-01-25 02:46:36 +0100

    Heat added changed style

--- Jump'n'Run_Collab/GOTY/Assets/Alex/HUDInterface.cs
+++ Jump'n'Run_Collab/GOTY/Assets/Alex/HUDInterface.cs
@@ -1,13 +1,18 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HUDInterface : MonoBehaviour {
     public  Image currentHealth;
-    public Text ratioText;
+    public Text ratioTextHp;
+    public Image currentHeat;
+    public Text ratioTextHeat;
     private static float maxHP = 100;
     private static float ratioHealth;
+    private static float maxHeat = 100;
+    private static float ratioHeat;
     public GameObject player;
     static NewPlayerMovement otherScript;
 
@@ -17,8 +22,15 @@ public class HUDInterface : MonoBehaviour {
 	
 	public  void updateHUD()
     {
+        //Health Stuff
         ratioHealth = maxHP / otherScript.Damage;
         currentHealth.rectTransform.localScale = new Vector3(ratioHealth, 1, 1);
+        ratioTextHp.text =(Math.Round(ratioHealth * 100)).ToString();
+
+        //Heat Stuff
+        //ratioHeat = maxHeat / otherScript.Heat;
+        currentHeat.rectTransform.localScale = new Vector3(ratioHeat, 1, 1);
+        ratioTextHeat.text = (Math.Round(ratioHeat * 100)).ToString();
     }
 
     
